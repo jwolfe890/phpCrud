be5a4104a5bc0b9d82948b86a7e24e421657a8d4
John Wolfe <jwolfe890@gmail.com>
2018-05-20 22:13:24 -0400

    styled form

--- index.php
+++ index.php
@@ -15,20 +15,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand crud" href="#">Navbar</a>
-  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button> -->
-
-  <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div> -->
 </nav>
 
 <ul class="nav nav-tabs ml-5 mt-3" role="tablist">
